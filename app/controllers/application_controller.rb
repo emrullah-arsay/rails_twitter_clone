5c632a4e5959a3c2ac33ba5cc9ea0ffa58d39ce4
emrullah arsay <emrixxx@gmail.com>
2016-07-14 15:18:31 +0300

    new fields added to devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,13 +6,21 @@ class ApplicationController < ActionController::Base
 
 
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
+ before_filter :configure_devise_permitted_parameters, if: :devise_controller?
 
   protected
 
-  def configure_permitted_parameters
-    added_attrs = [:rname,:description :email, :password, :password_confirmation, :remember_me]
-    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  def configure_devise_permitted_parameters
+    permitted_params = [:name,:description,:email, :password, :password_confirmation, :unit_prefs]
+
+    if params[:action] == 'update'
+      devise_parameter_sanitizer.for(:account_update) { 
+        |u| u.permit(permitted_params << :current_password)
+      }
+    elsif params[:action] == 'create'
+      devise_parameter_sanitizer.for(:sign_up) { 
+        |u| u.permit(permitted_params) 
+      }
+    end
   end
 end
